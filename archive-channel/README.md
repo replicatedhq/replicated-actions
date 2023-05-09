353f58430eb835d38bd87f49062d103683f15afd
Josh De Winne <josh@dewinne.dev>
2023-05-09 14:22:47 -0700

    Use alpha 12 from npm module

--- archive-channel/README.md
+++ archive-channel/README.md
@@ -8,5 +8,5 @@ graph LR
 archive_channel["Archive Channel"]
 replicated_app ---> archive_channel
 replicated_api_token ---> archive_channel
-channel_id ---> archive_channel
+channel_name ---> archive_channel
 ```
--- archive-channel/src/index.ts
+++ archive-channel/src/index.ts
@@ -1,10 +1,18 @@
 import * as core from '@actions/core';
 import { archiveChannel } from 'replicated-lib';
+import { VendorPortalApi } from 'replicated-lib/dist/configuration';
 
 
 async function run() {
   try {
-    await archiveChannel(core.getInput('replicated-app'), core.getInput('channel-name'))
+    const apiToken = core.getInput('replicated-api-token')
+    const appSlug = core.getInput('replicated-app')
+    const channelName = core.getInput('channel-name')
+    
+    const apiClient = new VendorPortalApi();
+    apiClient.apiToken = apiToken;
+    
+    await archiveChannel(apiClient, appSlug, channelName)
 
   } catch (error) {
     core.setFailed(error.message);
