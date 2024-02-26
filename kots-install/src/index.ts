33624095b1433264f05ce707c2dcd112d18af2fb
Craig O'Donnell <craig@replicated.com>
2024-02-26 15:39:45 -0500

    allow file path for license and config values

--- kots-install/src/index.ts
+++ kots-install/src/index.ts
@@ -5,18 +5,32 @@ import * as fs from 'fs';
 
 async function run() {
 
-  // Write the license
   let licenseFilePath = '';
-  const {fd, path: licensePath, cleanup: cleanupValues} = await file({postfix: '.yaml'});
-  fs.writeFileSync(licensePath, core.getInput('license-file'));
-  licenseFilePath = licensePath;
+  if (core.getInput('license-file')) {
+    // Write the license
+    const {path: licensePath} = await file({postfix: '.yaml'});
+    fs.writeFileSync(licensePath, core.getInput('license-file'));
+    licenseFilePath = licensePath;
+  } else {
+    // Fall back to the license file path
+    licenseFilePath = core.getInput('license-file-path');
+  }
 
-  // Write the values if any
+  // License file is required for an automated install
+  if (!licenseFilePath) {
+    core.setFailed('No license file provided. Please provide a license-file or a license-file-path.');
+    return;
+  }
+
   let valuesFilePath = '';
   if (core.getInput('config-values')) {
-    const {fd, path: valuesPath, cleanup: cleanupValues} = await file({postfix: '.yaml'});
+    // Write the values if any
+    const {path: valuesPath} = await file({postfix: '.yaml'});
     fs.writeFileSync(valuesPath, core.getInput('config-values'));
     valuesFilePath = valuesPath;
+  } else {
+    // Fall back to the values file path
+    valuesFilePath = core.getInput('config-values-path');
   }
 
 
