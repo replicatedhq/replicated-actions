a9098be77170dd73403d12d2e839c0ef44b3ccb5
Josh De Winne <joris.dewinne@gmail.com>
2026-04-23 09:50:58 -0700

    feat: wire create-vm and remove-vm into barrel export

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,13 @@ import { actionCreateCluster } from "./create-cluster";
 import { actionCreateCustomer } from "./create-customer";
 import { actionCreateObjectStore } from "./create-object-store";
 import { actionCreateRelease } from "./create-release";
+import { actionCreateVM } from "./create-vm";
 import { actionExposePort } from "./expose-port";
 import { actionHelmInstall } from "./helm-install";
 import { actionKotsInstall } from "./kots-install";
 import { actionPromoteRelease } from "./promote-release";
 import { actionRemoveCluster } from "./remove-cluster";
+import { actionRemoveVM } from "./remove-vm";
 import { actionGetCustomerInstances } from "./get-customer-instances";
 import { actionUpgradeCluster } from "./upgrade-cluster";
 import { actionReportCompatibilityResult } from "./report-compatibility-result";
@@ -20,11 +22,13 @@ export {
   actionCreateCustomer,
   actionCreateObjectStore,
   actionCreateRelease,
+  actionCreateVM,
   actionExposePort,
   actionHelmInstall,
   actionKotsInstall,
   actionPromoteRelease,
   actionRemoveCluster,
+  actionRemoveVM,
   actionGetCustomerInstances,
   actionUpgradeCluster,
   actionReportCompatibilityResult
